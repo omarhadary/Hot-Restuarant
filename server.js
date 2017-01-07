a803870cf3082fd5111ae3c89082c66b82aea230
Omar Elhadary <Omar Elhadary>
2017-01-07 11:17:04 -0800

    listening to server, created routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//NPM = express and body-parser
+//NPM dependencies
 
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -7,9 +7,37 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+// Express to handle parsing
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 var customer = [];
+
+//routes
+// Home page route
+app.get("/", function(require, result) {
+    result.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(require, result) {
+    result.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/view", function(require, result) {
+    result.sendFile(path.join(__dirname, "view.html"));
+});
+
+// searching for customers
+
+// creating new customers
+
+// api links at bottom of page
+
+// start server
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT)
+});

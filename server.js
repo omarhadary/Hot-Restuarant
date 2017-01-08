408f2aac9db00f8f9653f37a39903bff73e7adc5
parkjjustin <parkjjustin@gmail.com>
2017-01-08 06:11:09 -0800

    Testing preliminary back-end functions

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var tables = 3;
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+var reservation = [];
+
+var waitingList = [];
+
+app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname,"home.html"));
+});
+
+app.get("/add", function(request, response) {
+  response.sendFile(path.join(__dirname,"add.html"));
+});
+
+app.get("/view", function(request, response) {
+  response.sendFile(path.join(__dirname,"view.html"));
+});
+
+app.get("/api/tables", function(request, response) {
+  response.json(reservation);
+});
+
+app.post("/api/tables", function(request, response) {
+  var newcustomer = request.body;
+  reservation.push(newcustomer);
+  response.json(newcustomer)
+});
+
+///
+
